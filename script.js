da1274f3038b7d484b2d97c726d19c1a8955dce5
Tamal8471 <95556982+Tamal8471@users.noreply.github.com>
2022-03-07 09:33:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// // ---------------------values & variables-------------------------------
+// let firstName = "tamal"
+
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+
+// // ---------------------naming rules for variable---------------
+
+// let PI = "3.14"
+// let nam1 = "kunal"
+
+// // ----------------notes---------------
+// let country="india"
+// console.log(country)
+
+// let continent ="asia"
+// console.log(continent)
+
+// let population = "1.4 billion"
+// console.log(population)
+// -------------------naming rules of variable----------------
+// 1.variable name should start with a Character or underscore
+// ex
+let name ="khan"
+let _name ="khan"
+console.log(name)
+console.log(_name)
+// .2 A variable name cannot start with a digit or special character accept underscore or dollar
+// ex
+// let 12pollution ="noise"
+// console.log(12pollution)
+// .3 A variable name can only contain alpha-numeric characters and underscores (a-z, A-Z, 0-9, and _ )
+// ex
+let rule_12 ="pop"
+console.log(rule_12)
+// .4 Variable names are case-sensitive (age, Age and AGE are three different variables)
+// ex
+let age ="13"
+let Age ="14"
+let AGE ="16"
+console.log(age)
+console.log(Age)
+console.log(AGE)
+// error
+// let Age ="13"
+// let Age ="14"
+// console.log(Age)
+// console.log(Age)
+// 5.The variable name cannot be any Go keywords
+// ex
+// let function ="15"
+// console.log(function)
+
